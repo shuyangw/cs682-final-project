8dc10673bc6a5ff544f1892c1868d088a5136c5c
shuyangw <qwqw1337@gmail.com>
2018-11-09 14:30:32 -0500

    Remove logic.py

--- src/logic.py
+++
@@ -1,7 +0,0 @@
-class GameLogic(object):
-    """
-    GameLogic class handles interprets all of the actions from the game and 
-    performs actions into the game.
-    """
-    def __init__(self):
-        pass
