ccc751fbf36380d37d90a6e51d6e7c27608b8506
shuyangw <qwqw1337@gmail.com>
2018-11-09 19:53:08 -0500

    Add NN functionality

--- src/ddqn.py
+++ src/ddqn.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 class DDQNCNN(object):
     def __init__(self, state_size, action_size, lr, name="DDQN"):
@@ -7,98 +8,114 @@ class DDQNCNN(object):
         self.lr = lr
         self.name = name
 
-        self.value_fc = tf.layers.dense(inputs = self.flatten,
-                    units = 512,
-                    activation = tf.nn.elu,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="value_fc")
+        with tf.variable_scope(self.name):
             
-        self.value = tf.layers.dense(inputs = self.value_fc,
-                    units = 1,
-                    activation = None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="value")
-        
-        # Ccalculate A(s,a)
-        self.advantage_fc = tf.layers.dense(inputs = self.flatten,
-                    units = 512,    
-                    activation = tf.nn.elu,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="advantage_fc")
-        
-        self.advantage = tf.layers.dense(inputs = self.advantage_fc,
-                    units = self.action_size,
-                    activation = None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="advantages")
-        
-        # Agregating layer
-        # Q(s,a) = V(s) + (A(s,a) - 1/|A| * sum A(s,a'))
-        self.output = self.value + tf.subtract(self.advantage, 
-            tf.reduce_mean(self.advantage, axis=1, keepdims=True))
+            # We create the placeholders
+            # *state_size means that we take each elements of state_size in tuple hence is like if we wrote
+            # [None, 100, 120, 4]
+            self.inputs_ = tf.placeholder(tf.float32, [None, *state_size], name="inputs")
             
-        # Q is our predicted Q value.
-        self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis=1)
-        
-        # The loss is modified because of PER 
-        self.absolute_errors = tf.abs(self.target_Q - self.Q)
-        
-        self.loss = tf.reduce_mean(
-            self.ISWeights_ * tf.squared_difference(self.target_Q, self.Q))
-        
-        self.optimizer = tf.train.RMSPropOptimizer(
-            self.learning_rate).minimize(self.loss)
+            #
+            self.ISWeights_ = tf.placeholder(tf.float32, [None,1], name='IS_weights')
+            
+            self.actions_ = tf.placeholder(tf.float32, [None, action_size], name="actions_")
+            
+            # Remember that target_Q is the R(s,a) + ymax Qhat(s', a')
+            self.target_Q = tf.placeholder(tf.float32, [None], name="target")
+            
+            """
+            First convnet:
+            CNN
+            ELU
+            """
+            # Input is 100x120x4
+            self.conv1 = tf.layers.conv2d(inputs = self.inputs_,
+                                         filters = 32,
+                                         kernel_size = [8,8],
+                                         strides = [4,4],
+                                         padding = "VALID",
+                                          kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                         name = "conv1")
+            
+            self.conv1_out = tf.nn.elu(self.conv1, name="conv1_out")
+            
+            
+            """
+            Second convnet:
+            CNN
+            ELU
+            """
+            self.conv2 = tf.layers.conv2d(inputs = self.conv1_out,
+                                 filters = 64,
+                                 kernel_size = [4,4],
+                                 strides = [2,2],
+                                 padding = "VALID",
+                                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                 name = "conv2")
 
-class DDQNC(object):
-    def __init__(self, state_size, action_size, lr, name="DDQN"):
-        self.state_size = state_size
-        self.action_size = action_size
-        self.lr = lr
-        self.name = name
-
-        """
-        Traditionally, here is where we would implement CNN layers, we would
-        instead like to do it with capsule layers.
-        """ 
-
-        self.value_fc = tf.layers.dense(inputs = self.flatten,
-                    units = 512,
-                    activation = tf.nn.elu,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="value_fc")
+            self.conv2_out = tf.nn.elu(self.conv2, name="conv2_out")
             
-        self.value = tf.layers.dense(inputs = self.value_fc,
-                    units = 1,
-                    activation = None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="value")
-        
-        # Ccalculate A(s,a)
-        self.advantage_fc = tf.layers.dense(inputs = self.flatten,
-                    units = 512,    
-                    activation = tf.nn.elu,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="advantage_fc")
-        
-        self.advantage = tf.layers.dense(inputs = self.advantage_fc,
-                    units = self.action_size,
-                    activation = None,
-                    kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                    name="advantages")
-        
-        # Agregating layer
-        # Q(s,a) = V(s) + (A(s,a) - 1/|A| * sum A(s,a'))
-        self.output = self.value + tf.subtract(self.advantage, 
-            tf.reduce_mean(self.advantage, axis=1, keepdims=True))
             
-        # Q is our predicted Q value.
-        self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis=1)
-        
-        # The loss is modified because of PER 
-        self.absolute_errors = tf.abs(self.target_Q - self.Q)
-        
-        self.loss = tf.reduce_mean(
-            self.ISWeights_ * tf.squared_difference(self.target_Q, self.Q))
-        
-        self.optimizer = tf.train.RMSPropOptimizer(
-            self.learning_rate).minimize(self.loss)
+            """
+            Third convnet:
+            CNN
+            ELU
+            """
+            self.conv3 = tf.layers.conv2d(inputs = self.conv2_out,
+                                 filters = 128,
+                                 kernel_size = [4,4],
+                                 strides = [2,2],
+                                 padding = "VALID",
+                                kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+                                 name = "conv3")
+
+            self.conv3_out = tf.nn.elu(self.conv3, name="conv3_out")
+            
+            
+            self.flatten = tf.layers.flatten(self.conv3_out)
+
+
+            self.value_fc = tf.layers.dense(inputs = self.flatten,
+                        units = 512,
+                        activation = tf.nn.elu,
+                        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                        name="value_fc")
+                
+            self.value = tf.layers.dense(inputs = self.value_fc,
+                        units = 1,
+                        activation = None,
+                        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                        name="value")
+            
+            # Ccalculate A(s,a)
+            self.advantage_fc = tf.layers.dense(inputs = self.flatten,
+                        units = 512,    
+                        activation = tf.nn.elu,
+                        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                        name="advantage_fc")
+            
+            self.advantage = tf.layers.dense(inputs = self.advantage_fc,
+                        units = self.action_size,
+                        activation = None,
+                        kernel_initializer=tf.contrib.layers.xavier_initializer(),
+                        name="advantages")
+            
+            # Agregating layer
+            # Q(s,a) = V(s) + (A(s,a) - 1/|A| * sum A(s,a'))
+            self.output = self.value + tf.subtract(self.advantage, 
+                tf.reduce_mean(self.advantage, axis=1, keepdims=True))
+                
+            # Q is our predicted Q value.
+            self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions_), axis=1)
+            
+            # The loss is modified because of PER 
+            self.absolute_errors = tf.abs(self.target_Q - self.Q)
+            
+            self.loss = tf.reduce_mean(
+                self.ISWeights_ * tf.squared_difference(self.target_Q, self.Q))
+            
+            self.optimizer = tf.train.RMSPropOptimizer(
+                self.learning_rate).minimize(self.loss)
+    
+    def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
+
--- src/runner.py
+++ src/runner.py
@@ -3,6 +3,8 @@ from actions import GameActions
 
 import time 
 
+import tensorflow as tf
+
 class Runner(object):
     """
     This is the class that performs every action that we need depending on the
@@ -20,8 +22,8 @@ class Runner(object):
      - duration: An integer representing how long we would like our agent to
        run in seconds.
     """
+
     def run(self, duration=60):
-        print("Runner running") 
         timer = time.time()
         GR = GameReader(self.fps, 15)
         GA = GameActions()
@@ -31,4 +33,123 @@ class Runner(object):
             iteration += 1
             GR.update_frames()
 
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        print("Runner running") 
+        # Initialize the variables
+        sess.run(tf.global_variables_initializer())
+        
+        # Initialize the decay rate (that will use to reduce epsilon) 
+        decay_step = 0
+
+        for episode in range(total_episodes):
+            # Set step to 0
+            step = 0
             
+            # Initialize the rewards of the episode
+            episode_rewards = []
+            
+            # Make a new episode and observe the first state
+            state = game.get_state().screen_buffer
+            
+            # Remember that stack frame function also call our preprocess function.
+            state, stacked_frames = stack_frames(stacked_frames, state, True)
+
+            while step < max_steps:
+                step += 1
+                
+                # Increase decay_step
+                decay_step +=1
+                
+                # Predict the action to take and take it
+                action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, possible_actions)
+
+                # Do the action
+                reward = game.make_action(action)
+
+                # Look if the episode is finished
+                done = game.is_episode_finished()
+                
+                # Add the reward to total reward
+                episode_rewards.append(reward)
+
+                # If the game is finished
+                if done:
+                    # the episode ends so no next state
+                    next_state = np.zeros((84,84), dtype=np.int)
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+
+                    # Set step = max_steps to end the episode
+                    step = max_steps
+
+                    # Get the total reward of the episode
+                    total_reward = np.sum(episode_rewards)
+
+                    print('Episode: {}'.format(episode),
+                              'Total reward: {}'.format(total_reward),
+                              'Training loss: {:.4f}'.format(loss),
+                              'Explore P: {:.4f}'.format(explore_probability))
+
+                    memory.add((state, action, reward, next_state, done))
+
+                else:
+                    # Get the next state
+                    next_state = game.get_state().screen_buffer
+                    
+                    # Stack the frame of the next_state
+                    next_state, stacked_frames = stack_frames(stacked_frames, next_state, False)
+                    
+
+                    # Add experience to memory
+                    memory.add((state, action, reward, next_state, done))
+                    
+                    # st+1 is now our current state
+                    state = next_state
+
+
+                ### LEARNING PART            
+                # Obtain random mini-batch from memory
+                batch = memory.sample(batch_size)
+                states_mb = np.array([each[0] for each in batch], ndmin=3)
+                actions_mb = np.array([each[1] for each in batch])
+                rewards_mb = np.array([each[2] for each in batch]) 
+                next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+                dones_mb = np.array([each[4] for each in batch])
+
+                target_Qs_batch = []
+
+                 # Get Q values for next_state 
+                Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: next_states_mb})
+                
+                # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+                for i in range(0, len(batch)):
+                    terminal = dones_mb[i]
+
+                    # If we are in a terminal state, only equals reward
+                    if terminal:
+                        target_Qs_batch.append(rewards_mb[i])
+                        
+                    else:
+                        target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+                        target_Qs_batch.append(target)
+                        
+
+                targets_mb = np.array([each for each in target_Qs_batch])
+
+                loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+                                    feed_dict={DQNetwork.inputs_: states_mb,
+                                               DQNetwork.target_Q: targets_mb,
+                                               DQNetwork.actions_: actions_mb})
+
+                # Write TF Summaries
+                summary = sess.run(write_op, feed_dict={DQNetwork.inputs_: states_mb,
+                                                   DQNetwork.target_Q: targets_mb,
+                                                   DQNetwork.actions_: actions_mb})
+                writer.add_summary(summary, episode)
+                writer.flush()
+
+            # Save model every 5 episodes
+            if episode % 5 == 0:
+                save_path = saver.save(sess, "./models/model.ckpt")
+                print("Model Saved")
